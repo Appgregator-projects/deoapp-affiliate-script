3b9505f5f9c179cedb7ec2428fd5f8b0f8cd4cda
faizaledrus <faizal.edrus@gmail.com>
2024-05-04 20:48:45 +0700

    change back to affiliateid.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './affiliateid.js',
   output: {
-    filename: 'index.js',
+    filename: 'affiliateid.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
